1c3ca5a9d4bbee3523b929f8a9af0b9231f5a537
TFS-Connector <58073697+tfs-connector@users.noreply.github.com>
2019-11-22 12:22:37 +0000

    Message Box
    git-tfs-id: [http://tfs:8080]$/Test Project;C8

---
+++ Solution-branch/Test Project/Test Project/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            (new AboutBox()).ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello " + textBox1.Text);
+        }
+    }
+}

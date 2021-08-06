08bbf9918e530590668f2d2f8af01db8ec6c3547
TFS-Connector <58073697+tfs-connector@users.noreply.github.com>
2021-08-06 00:19:42 +0000

    Fix Bug 92
    git-tfs-id: [http://tfs:8080]$/Test Project;C64

---
+++ Solution/Test Project/ClassLibrary1/Greeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Greeter
+    {
+        public string HelloMessage(string name)
+        {
+            return "Hello "+name+"!";
+        }
+    }
+}
